161efd13bee747b5789e767b842c6b5d2392e766
Thomas Cooper <thomascooperca@gmail.com>
2022-10-31 02:24:31 -0400

    use set seed for ui

--- python/main.py
+++ python/main.py
@@ -15,27 +15,22 @@ background = pyglet.shapes.Rectangle(x=0, y=0, width=720, height=720, color=(64,
 shape_batch = pyglet.shapes.Batch()
 cell_grid = []
 r = random.Random()
-seeded_cells = [(r.randint(0, 23), r.randint(0, 23)) for _ in range(5)]
-print(seeded_cells)
+# seeded_cells = [(r.randint(0, 23), r.randint(0, 23)) for _ in range(20)]
+# print(seeded_cells)
+g = [[False, False, True, False, False, False, False, False, True, False, True, False, False, False, True, True, True, True, True, False, False, False, False, True], [True, True, False, False, True, False, False, True, True, True, False, False, True, True, False, True, False, False, True, True, False, True, True, False], [True, True, False, True, True, False, False, False, True, False, True, True, True, True, False, True, False, True, True, True, False, True, False, False], [False, False, False, True, False, False, True, True, True, False, False, True, False, True, True, False, True, False, False, False, False, True, False, False], [False, True, True, False, False, False, False, True, False, False, False, False, False, False, False, True, False, True, False, False, False, True, False, True], [True, False, False, False, True, False, True, True, True, False, True, True, True, False, False, True, False, False, False, False, False, True, False, False], [False, False, True, True, False, True, False, True, True, False, False, False, True, False, True, True, False, True, False, True, False, True, True, False], [True, True, False, True, True, False, False, True, False, True, False, False, True, True, False, True, True, False, True, False, False, False, True, True], [True, True, False, True, False, False, False, False, False, False, False, True, False, False, False, True, False, True, False, True, True, True, True, False], [False, False, True, True, True, False, True, True, False, True, False, True, False, False, False, False, True, True, True, False, False, False, False, True], [False, True, True, False, True, True, True, False, True, True, True, False, True, False, False, True, True, False, False, True, True, True, True, True], [True, False, False, False, False, False, False, True, False, True, True, False, False, False, True, True, True, False, True, False, False, False, False, False], [False, True, False, True, False, False, False, False, False, True, False, True, True, False, False, True, True, True, True, False, True, True, True, False], [False, True, False, True, False, True, False, True, True, True, True, True, False, False, True, True, True, True, True, False, False, True, False, False], [False, True, True, False, True, False, False, True, True, True, False, False, False, False, True, True, False, False, False, True, True, False, True, False], [True, True, False, True, False, False, True, False, True, True, True, False, True, False, False, True, True, True, True, True, False, False, True, False], [True, False, False, True, True, True, True, False, False, False, False, True, False, False, True, False, False, True, False, False, False, True, False, False], [False, True, True, False, False, False, True, True, False, False, True, True, False, True, True, True, True, False, False, True, True, False, False, True], [True, False, True, True, False, False, False, True, False, False, True, True, True, True, True, False, True, False, False, False, False, True, False, False], [True, False, True, True, True, True, True, True, False, True, False, True, False, False, False, True, True, True, False, False, False, False, True, False], [False, False, True, False, False, True, False, True, False, True, True, False, False, True, True, False, True, True, True, False, False, False, True, False], [True, True, True, True, False, True, False, True, True, False, True, True, False, True, True, True, True, True, True, True, True, True, True, True], [True, False, True, True, True, True, True, True, True, True, False, True, False, False, True, False, False, True, True, True, True, False, False, True], [True, False, False, True, True, True, True, False, True, False, True, True, False, True, False, True, False, False, True, False, True, False, True, False]]
+
 for i in range(DIMENSION):
     cell_grid.append([])
     for j in range(DIMENSION):
-        colour = DEAD_COLOUR
-        print(f"i: {i}, j: {j}")
-        for s in seeded_cells:
-            if i == s[0] and j == s[1]:
-                colour = LIVE_COLOUR
+        # colour = DEAD_COLOUR
+        # print(f"i: {i}, j: {j}")
+        # for s in seeded_cells:
+        #     if i == s[0] and j == s[1]:
+        #         colour = LIVE_COLOUR
         cell_grid[i].append(pyglet.shapes.Rectangle(x=29 * j + 5, y=29 * i + 5, width=22, height=22,
-                                                 color=colour, batch=shape_batch))
-
-
-# frame = pyglet.gui.Frame(window, order=1)
-# go_button = pyglet.gui.PushButton(x=720 // 2, y=721, pressed=pyglet.resource.image('rocket_small.png'),
-#                                   depressed=pyglet.resource.image('go-button.png'),
-#                                   hover=pyglet.resource.image('go-button-down.png'),
-#                                   batch=shape_batch)
-# go_button.set_handler('on_press', activate_update)
-# frame.add_widget(go_button)
+                                                    # color=colour,
+                                                    color=LIVE_COLOUR if g[i][j] else DEAD_COLOUR,
+                                                    batch=shape_batch))
 
 
 def mouse_intersects_cell(x, y, rect: pyglet.shapes.Rectangle) -> bool:
@@ -58,19 +53,38 @@ def is_live(rect: pyglet.shapes.Rectangle):
     return rect.color == LIVE_COLOUR
 
 
-MAX_BACKOFF = 20
+MAX_BACKOFF = 4
 backoff = MAX_BACKOFF
 
-
 def update_cells(dt):
+    pass
+
+
+def _update_cells(dt):
     global backoff
     print(f"Called update at {dt} (backoff: {backoff})")
     if (0 < backoff <= MAX_BACKOFF):
         backoff -= 1
         return
     print("Update cells")
-    #TODO: Slice cell_grid in 3x3 md arrays and use life functions
-    #TODO: Convert 3x3 md array to cell_grid entries
+    #TODO: Need to convert every rect cell to a bool
+    bools = []
+    for i in range(DIMENSION):
+        bools.append([])
+        for j in range(DIMENSION):
+            bools[i].append(cell_grid[i][j].color == LIVE_COLOUR)
+
+    c1 = [ bools[0][0:3] ]
+    c1 += [ bools[1][0:3] ]
+    c1 += [ bools[2][0:3] ]
+    print(c1)
+    c1 = life.update(c1, life.should_live(c1))
+    print(c1)
+    bools[0:3] = c1[0:3]
+    for i in range(3):
+        for j in range(3):
+            cell_grid[i][j].color = LIVE_COLOUR if bools[i][j] else DEAD_COLOUR
+
     # for i in range(DIMENSION):
     #     this_group = [ cell_grid[i][i:i+3] ]
     #     for j in range(0, DIMENSION, 3):
